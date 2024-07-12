1751469884961ed1131df2489db43c787d5d1c5a
sinedied <noda@free.fr>
2024-07-12 14:02:27 +0200

    refactor: simplify summarizer

--- src/07-summarizer.js
+++ src/07-summarizer.js
@@ -1,8 +1,7 @@
-// This example demonstrates how to summarize a large text using a controllable
-// level of detail.
+// This example demonstrates how to summarize a large text.
 //
-// NOTE: This example requires more time to run, as it takes a large text as
-// input and makes multiple requests to the OpenAI API to summarize it.
+// NOTE: This example requires more time to run, as the context size is very
+// large.
 
 import fs from "node:fs";
 import { OpenAI } from "openai";
@@ -12,59 +11,20 @@ const systemPrompt = `Rewrite this text into a very brief summary.`;
 // Load a large text
 const text = fs.readFileSync("./data/ai_wikipedia.txt", "utf8");
 
-// Split a text into an array of smaller chunks
-const chunkText = (text, minChars, delimiter = '.') => {
-  const chunks = [];
-  let currentChunk = "";
-  for (const section of text.split(delimiter)) {
-    if (currentChunk.length > minChars) {
-      chunks.push(currentChunk);
-      currentChunk = "";
-    }
-    currentChunk += section + delimiter;
-  }
-  chunks.push(currentChunk);
-  return chunks;
-};
+const openai = new OpenAI({
+  baseURL: "http://localhost:11434/v1",
+  apiKey: "__not_needed_by_ollama__",
+});  
 
-// Summarize a text using specified detail level
-// 0: least detailed, 1: most detailed
-const summarize = async (text, detail = 0) => {
-  const summaries = [];
+const result = await openai.chat.completions.create({
+  model: "phi3",
+  messages: [
+    { role: "system", content: systemPrompt },
+    { role: "user", content: text },
+  ],
+});
+const summary = result.choices[0].message.content;
 
-  // Interpolate the number of chunks based to get specified level of detail
-  const maxChunks = text.split('\n\n').length;
-  const numChunks = Math.floor(1 + detail * (maxChunks - 1));
-
-  // Adjust chunk size based on interpolated number of chunks
-  const chunkSize = Math.max(1000, Math.floor(text.length / numChunks));
-  const chunks = chunkText(text, chunkSize);
-
-  console.log(`Splitting the text into ${chunks.length} chunks to be summarized.`);
-  console.log(`Chunk sizes: ${chunks.map((c) => c.length)}`);
-
-  const openai = new OpenAI({
-    baseURL: "http://localhost:11434/v1",
-    apiKey: "__not_needed_by_ollama__",
-  });  
-
-  // Generate summaries for each chunk
-  for (const chunk of chunks) {
-    const result = await openai.chat.completions.create({
-      model: "phi3",
-      messages: [
-        { role: "system", content: systemPrompt },
-        { role: "user", content: chunk },
-      ],
-    });
-
-    summaries.push(result.choices[0].message.content);
-  }
-
-  return summaries.join("\n\n");
-};
-
-const summary = await summarize(text, 0.2);
-
+console.log(`Original text: ${text.length} chars`);
 console.log(`Summarized text (${summary.length} chars):`);
 console.log(summary);
